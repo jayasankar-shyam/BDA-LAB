0adeae280f885d26f0185dc4dd80bd87171e2894
Jayasankar Shyam <84973869+jayasankar-shyam@users.noreply.github.com>
2023-02-27 12:38:22 +0530

    Add files via upload

---
+++ multiples.R
@@ -0,0 +1,9 @@
+ans=i=1
+while (ans<100){
+  ans=5*i
+  print(ans)
+  i=i+1
+}
+
+
+
--- read.R
+++ read.R
@@ -1,3 +1,5 @@
 name=readline(prompt = "Enter your name : ")
 age=as.integer(readline(prompt = "Enter your age : "))
 print(paste("Hi",name,"you are",age,"years old"))
+
+
